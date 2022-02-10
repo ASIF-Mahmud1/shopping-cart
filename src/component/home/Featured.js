201f336c9099c00453d28a879cde57b111d022b1
Asif <1421015@iub.edu.bd>
2022-02-10 22:03:46 +0600

    states are  moved up to Home component

--- src/component/home/Featured.js
+++ src/component/home/Featured.js
@@ -5,53 +5,9 @@ import {itemList} from '../../../assets/data/items'
 import {storeData,getData} from '../../helper/helper'
 import { useIsFocused } from '@react-navigation/native';
 
-export default function Featured() {
-  const [items, setItem]=useState([]) 
+export default function Featured({items,handleAddToCart}) {
 
-  useEffect(()=>{
-    const result= itemList.map((ele)=>{return {...ele, added: false}})
-    getData("cartList",(value)=>{
-      if(value!==null)
-      {
-      
-        for(let i=0;i<value.length;i++)
-        {
-           let idx= result.findIndex((ele)=> ele.id=== value[i].id)
-           if(idx!=-1)
-           {
-            result[idx]= {...result[idx], added:true}
-           }
-        }
-     
-      }
-      setItem(result)
-
-    })
-         
-  },[useIsFocused()])
-
-  const handleAddToCart=(id)=>{
-   const result= items.map((ele)=>{
-     if(ele.id===id)
-     {
-        return {
-          ...ele,
-          added: !ele.added
-        }
-     }
-     else
-     {
-       return ele
-     }
-   })
-
-      
-   const forCart= result.filter((ele)=>ele.added===true)
-   storeData("cartList", forCart)
-
-   setItem(result)
-
-  }
+  
   return (
     <View style={styles.container}>
       <Text>Featured Component</Text>
--- src/container/Home.js
+++ src/container/Home.js
@@ -1,27 +1,82 @@
-import React from 'react';
+import React,  { useState,useEffect } from 'react';
 
 import { StyleSheet, Text, View, TouchableOpacity ,StatusBar, Button,ScrollView} from 'react-native';
 import Category from '../component/home/Category'
 import Featured from '../component/home/Featured'
 import BestSale from '../component/home/BestSale'
-
+import {itemList} from '../../assets/data/items'
+import {storeData,getData} from '../helper/helper'
+import { useIsFocused } from '@react-navigation/native';
 
 
 export default function Home({navigation}) {
+
+  const [items, setItem]=useState([]) 
+  useEffect(()=>{
+    const result= itemList.map((ele)=>{return {...ele, added: false}})
+    getData("cartList",(value)=>{
+      if(value!==null)
+      {
+      
+        for(let i=0;i<value.length;i++)
+        {
+           let idx= result.findIndex((ele)=> ele.id=== value[i].id)
+           if(idx!=-1)
+           {
+            result[idx]= {...result[idx], added:true}
+           }
+        }
+     
+      }
+      setItem(result)
+
+    })
+         
+  },[useIsFocused()])
+
+  const handleAddToCart=(id)=>{
+    const result= items.map((ele)=>{
+      if(ele.id===id)
+      {
+         return {
+           ...ele,
+           added: !ele.added
+         }
+      }
+      else
+      {
+        return ele
+      }
+    })
+ 
+       
+    const forCart= result.filter((ele)=>ele.added===true)
+    storeData("cartList", forCart)
+ 
+    setItem(result)
+ 
+   }
   const handleNavigation=()=>{
     navigation.navigate('Checkout')
   }
+  const cartSize= result.filter((ele)=>ele.added===true).length
+ 
   return (
     <View style={styles.container}>
         <ScrollView style={{ margin: 20 }}>
           <Category/>
-          <Featured/>
+          <Featured items={items}  handleAddToCart={handleAddToCart} />
           <BestSale/>
        </ScrollView>
-       <TouchableOpacity  onPress={handleNavigation} style={styles.checkOut} >
+       {
+
+         cartSize >0 && 
+         <TouchableOpacity  onPress={handleNavigation} style={styles.checkOut} >
          <Text style={styles.text}> Checkout </Text>
          </TouchableOpacity>
 
+       }
+       
     </View>
   );
 }
