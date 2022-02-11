600ec490a399b939acdab4356575a2cc68a5a7b7
Asif <1421015@iub.edu.bd>
2022-02-11 21:12:36 +0600

    [done] user details added

--- src/component/checkout/UserDetails.js
+++ src/component/checkout/UserDetails.js
@@ -2,19 +2,23 @@ import React from 'react';
 
 import { StyleSheet, Text, View, TouchableOpacity ,StatusBar} from 'react-native';
 
-export default function UserDetails({userDetails}) {
-
+export default function UserDetails({details}) {
+   const {name, address}=details
   return (
     <View style={styles.container}>
-      <Text>User Details Component</Text>
-  
-      <StatusBar style="auto" />
+      <Text style={styles.text} >Customer Details </Text>
+       <Text style={styles.text}>Name: {name}</Text>
+       <Text style={styles.text}>Address: {address}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
- 
+    paddingVertical:5,
   },
+  text:{
+    fontSize:14,
+    fontWeight:'500'
+  }
 });
