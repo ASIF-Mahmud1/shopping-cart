362159b81e18843a99bf549f0a2b979e7c79c62f
Asif <1421015@iub.edu.bd>
2022-03-10 16:11:41 +0600

    [in progres]  ui of category list

--- src/component/home/Category.js
+++ src/component/home/Category.js
@@ -1,9 +1,11 @@
 import React,{Fragment} from 'react';
 
-import { StyleSheet, Text, View, TouchableOpacity ,StatusBar,ScrollView,Image} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity ,StatusBar,ScrollView,Image,ImageBackground} from 'react-native';
 import {getListOfCategory} from '../../helper/helper'
 import {ItemCategory}from '../../../assets/data/items'
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import LinearGradient from 'react-native-linear-gradient';
+import GradientButton from '../../helper/component/GradientButton';
 
 export default function Category({items, handleNavigation}) {
  
@@ -21,9 +23,21 @@ const catagoryList= Object.keys(result)
           catagoryList.map((item) => {
             return (
 
-              <TouchableOpacity onPress={()=>handleNavigation(item,result[item])} style={styles.btn} >
-                  <Text style={styles.text}> {item} </Text>
-                  <Image source={{ uri:ItemCategory[item] }} style={styles.image} />
+              <TouchableOpacity onPress={() => handleNavigation(item, result[item])} style={styles.btn} >
+                <ImageBackground source={{ uri: ItemCategory[item] }} resizeMode="cover" style={styles.image} >
+
+                  <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#8466EA', '#64B6FF']} style={{  borderRadius: 30,}}>
+                    <View style={{
+                      width: 140,
+                      height: 100,
+                     justifyContent:'center',
+                   
+                    }}>
+                      <Text style={styles.imageText}>{item}</Text>
+                    </View>
+                  </LinearGradient>
+                </ImageBackground>
+
               </TouchableOpacity>
 
             )
@@ -79,14 +93,28 @@ const styles = StyleSheet.create({
   },
   image:{
     margin: 15,
-    padding: 30,
+   // padding: 30,
     width: 140,
     height: 100,
-    borderRadius: 30
+    borderRadius: 30,
+    opacity:0.7,
+    justifyContent:'center',
+    alignItems:'center',
+ 
+
   },
   btn:{
     paddingBottom:10,
-    alignItems: 'center'
+   // alignItems: 'center',
+    borderRadius: 30,
+    
+  },
+  imageText:{
+    textAlign:'center',
+    color:'white',
+    fontSize:20,
+    fontWeight:'bold',
+    fontStyle:"italic"
   }
 
 });
